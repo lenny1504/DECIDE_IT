72df7c60650bc436ed8909aa0fd0d41985834935
Lennart von Velsen <lennartvonvelsen@gmail.com>
2022-03-01 16:27:53 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :proposals do
+    resources :approval_flows, only: [:create, :new] do
+      resources :steps, only: [:create, :edit, :new, :update] do
+        resources :comments, [:create, :new]
+      end
+    end
+  end
+resources :steps, only: [:destroy]
+get “dashboard”, to: 'pages#dashboard'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
