bf59a526e05f18587fe6f59aaf1172eb68b9ffa3
Pablo Wilner <pablowilner@gmail.com>
2022-03-01 17:58:16 +0100

    approval flows first steps

--- app/controllers/approval_flows_controller.rb
+++ app/controllers/approval_flows_controller.rb
@@ -1,2 +1,30 @@
 class ApprovalFlowsController < ApplicationController
+
+  def new
+    # action triggered from the show of the proposals
+    @approval_flow = ApprovalFlow.new
+    # @proposal = Proposal.find(params[:proposal_id])
+  end
+
+  def create
+    @approval_flow = ApprovalFlow.new(approval_flow_params)
+    @approval_flow.creator = current_user
+    @approval_flow.proposal = Proposal.find(params[:proposal_id])
+    if @approval_flow.save
+      redirect_to approval_flow_path(@approval_flow), notice: "Your approval flow has been created"
+    else
+      render "approval_flow/new"
+    end
+  end
+
+  def show
+    @approval_flow = ApprovalFlow.find(params[:id])
+  end
+
+  private
+
+  def approval_flow_params
+    params.require(:approval_flow).permit(:name, :description)
+  end
+
 end
