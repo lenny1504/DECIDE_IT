bf22b2f4bfe9c042d4aa3f23b0c92f766edc59ad
Lennart von Velsen <lennartvonvelsen@gmail.com>
2022-03-01 17:58:59 +0100

    Proposals Controller

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,12 +1,30 @@
 class ProposalsController < ApplicationController
 
-  # def create
-  #   @proposal = Proposal.new(proposal_params)
+  def create
+    @proposal = Proposal.new(proposal_params)
+    @proposal.creator = current_user
+    if @proposal.save
+      redirect_to @proposal_path(@proposal.id), notice: 'Proposal was successfully created.'
+    else
+      render :new
+    end
+  end
 
-  #   if @proposal.save
-  #     redirect_to @restaurant, notice: 'Restaurant was successfully created.'
-  #   else
-  #     render :new
-  #   end
-  # end
+  def update
+    if @proposal.update(proposal_params)
+      redirect_to @proposal_path(@character)
+    else
+      render :edit
+    end
+  end
+
+  def archive
+    
+  end
+
+  private
+
+  def proposal_params
+    params.require(:proposal).permit(:title, :description, :status)
+  end
 end
