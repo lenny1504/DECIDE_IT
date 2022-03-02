576f2195a47ab76a1f7fdf48d2a066fa15814df1
Pablo Wilner <pablowilner@gmail.com>
2022-03-02 14:29:15 +0100

    seeds

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,6 +1,6 @@
 class Step < ApplicationRecord
   belongs_to :approval_flow
-  belongs_to :approver, class: "User", foreign_key: :user_id, primary_key: :id
+  belongs_to :approver, class_name: "User", foreign_key: :user_id, primary_key: :id
   has_many :logs
   has_many :comments
   validates :status, presence: true, inclusion: { in: ["created", "change request", "approved", "rejected"] }
