14893cbb24b49f4075ef3ef1b5fba39bf790ccea
ana rebelo <anarebelosoares@gmail.com>
2022-03-02 14:55:17 +0100

    working on comments controller & view

--- app/models/approval_flow.rb
+++ app/models/approval_flow.rb
@@ -2,5 +2,6 @@ class ApprovalFlow < ApplicationRecord
   belongs_to :creator, class: "User", foreign_key: :user_id, primary_key: :id
   belongs_to :proposal
   has_many :steps
+  has_many :comments through: :steps # @approval_flow.comment
   validates :name, presence: true
 end
