5dda2a917bcbce06f3016d7aa4ee88f36819cabe
Pablo Wilner <pablowilner@gmail.com>
2022-03-02 15:20:16 +0100

    some fixes

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -8,7 +8,7 @@ class ProposalsController < ApplicationController
     @proposal = Proposal.new(proposal_params)
     @proposal.creator = current_user
     if @proposal.save
-      redirect_to @proposal_path(@proposal.id), notice: 'Proposal was successfully created.'
+      redirect_to proposal_path(@proposal.id), notice: 'Proposal was successfully created.'
     else
       render :new
     end
@@ -16,7 +16,7 @@ class ProposalsController < ApplicationController
 
   def update
     if @proposal.update(proposal_params)
-      redirect_to @proposal_path(@character)
+      redirect_to proposal_path(@character)
     else
       render :edit
     end
